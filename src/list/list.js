30f0c3efcef79554de6e75b46e8fea629fac3858
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2021-10-15 16:40:24 -0600

    inital commit

---
+++ src/list/list.js
@@ -0,0 +1,10 @@
+const List = () => {
+  return (
+    <div>
+      <input type="checkbox" id="idProp" value="this will be a list item" />
+      <label for="idProp">something that looks like the value</label>
+    </div>
+  );
+};
+
+export default List;
